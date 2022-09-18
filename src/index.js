b0a1706d1404cf5b725e4bee81fcd6f1cb234dd7
Tayyab Mughal <tayyab.8563@gmail.com>
2022-09-18 08:59:05 +0500

    convert into scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'css/index.css';
+import 'css/style.scss';
 import App from 'app/App';
 import reportWebVitals from './reportWebVitals';
 
